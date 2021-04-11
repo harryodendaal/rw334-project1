1aca7e4dd00254d0388510548c973e6f4b03dc97
HARRY WILLY Odendaal <22794484@sun.ac.za>
2021-04-11 11:57:07 +0200

    removed comments

--- main.py
+++ main.py
@@ -2,27 +2,6 @@ from scraper import github_api, scrape_github
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-# data = [
-# {
-#   'repo_name':'openvc/opencv',
-#   'description':'Open Source Computer Vision Library',
-#   'num_stars':52800,
-#   'language':'C++',
-#   'license':None,
-#   'last_updated':'2021-03-07T20:18:592',
-#   'has_issues':True
-# },
-# {
-#   'repo_name':'openvc/opencv',
-#   'description':'Open Source Computer Vision Library',
-#   'num_stars':300,
-#   'language':'python',
-#   'license':'BSD-3-Clause "New" or "Revised" License',
-#   'last_updated':'2021-03-07T20:18:592',
-#   'has_issues':True
-# }
-# ]
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
   
